ead5554f06afa3a7973ce50d3099b3ca0009d6c0
Developer Rifat <itscrifat5147@gmail.com>
2022-11-10 02:27:38 +0600

    initial

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = "frontend"
+
 urlpatterns = [
     path('', views.Home, name="home"),
     path('subcategory/<int:cat_id>', views.SubcategoryView, name="subcategory"),
