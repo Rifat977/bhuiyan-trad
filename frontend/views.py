d7669c5f78e47162c74c66f3e8e0bc9e0076279d
Developer Rifat <itscrifat5147@gmail.com>
2022-11-10 00:33:43 +0600

    initial

--- frontend/views.py
+++ frontend/views.py
@@ -11,3 +11,15 @@ def Home(request):
         'product' : product
     }
     return render(request, 'home.html', context)
+
+def Subcategory(request):
+    settings = Settings.objects.first()
+    category = Category.objects.all().order_by('-id')
+    product = Product.objects.all()
+    context = {
+        'settings' : settings,
+        'category' : category,
+        'product' : product
+    }
+    return render(request, 'subcategory.html', context)
+
---
+++ static/frontend/assets/js/home.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    autoplay: {
+        delay: 3000,
+    },
+
+    breakpoints: {
+        "@0.75": {
+            slidesPerView: 1,
+            spaceBetween: 40,
+        },
+        "@1.00": {
+            slidesPerView: 2,
+            spaceBetween: 100,
+        },
+        "@1.50": {
+            slidesPerView: 3,
+            spaceBetween: 200,
+        },
+    },
+});
+
+const swipers = document.querySelector(".swiper").swiper;
+
+// Now you can use all slider methods like
+swipers.slideNext();
